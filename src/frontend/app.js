253455ad888145848e8286f81d29121b28e37962
imeraj-leon-emf <143509308+imeraj-leon-emf@users.noreply.github.com>
2025-02-19 08:45:39 +0100

    Update app.js

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,4 +1,4 @@
-const apiEndpoint = "azuresite.leonimeraj.emf-informatique.ch";
+const apiEndpoint = "https://container-leon-imeraj.calmpond-02ae4ede.switzerlandnorth.azurecontainerapps.io/api/tasks";
 
 $(document).ready(function () {
   // Charger les tâches au démarrage
